874a4ad4c8398acd454238b8a2cc83b73c83306c
doridoridolldoll <bluestar181818@gmail.com>
2022-05-29 17:20:29 +0900

    commit

--- back.js
+++ back.js
@@ -1,41 +1,43 @@
 const express = require('express')
 const fs = require('fs')
-const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: false }))
-
-app.get('/', function (req, res) {
-  if (req.cookies.auth) {
-    res.send('<h1>Login success</h1>')
-  } else {
-    res.redirect('/login')
-  }
-})
 
 app.get('/login', function (req, res) {
   fs.readFile('index.html', function (err, data) {
     res.send(data.toString())
   })
 })
+app.get('/main', function (req, res) {
+  fs.readFile('regi.html', function (err, data) {
+    res.send(data.toString())
+  })
+})
+app.get('/welcome', function (req, res) {
+    fs.readFile('welcome.html', function (err, data) {
+      res.send(data.toString())
+    })
+  })
 
 app.post('/login', function (req, res) {
   const id = req.body.id
-  const password = req.body.pw
+  const password = req.body.pass
 
   console.log(id, password)
   console.log(req.body)
-
-  if (id == 'admin' && password == '1234') {
-    res.cookie('auth', true)
-    res.redirect('/')
-  } else {
-    res.redirect('/login')
-  }
 })
+app.post('/main', function (req, res) {
+    const id = req.body.id
+    const password = req.body.pass
+    const name = req.body.name
+    const repass = req.body.repass
+  
+    console.log(name, id, password, repass)
+    console.log(req.body)
+  })
 
 app.listen(52273, function () {
   console.log('server is running')
